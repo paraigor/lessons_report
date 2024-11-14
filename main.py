42e9e30084f1fc58f4ab35a6f31f930112c9513e
paraigor <103633539+paraigor@users.noreply.github.com>
2024-11-14 21:34:50 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+import time
+
+import requests
+import telegram
+from environs import Env
+
+
+def request_attempts(url, headers, params, timeout):
+    response = requests.get(
+        url, headers=headers, params=params, timeout=timeout
+    )
+    response.raise_for_status()
+
+    return response.json()
+
+
+def main():
+    env = Env()
+    env.read_env()
+
+    api_token = env("API_TOKEN")
+    bot_token = env("BOT_TOKEN")
+    chat_id = env("CHAT_ID")
+    request_timeout = env.int("REQUEST_TIMEOUT")
+
+    bot = telegram.Bot(bot_token)
+
+    header = {"Authorization": f"Token {api_token}"}
+    url = "https://dvmn.org/api/long_polling/"
+
+    curr_timestamp = time.time()
+    payload = {"timestamp": curr_timestamp}
+
+    while True:
+        try:
+            response = request_attempts(url, header, payload, request_timeout)
+
+            if response["status"] == "found":
+                lesson_attempt = response["new_attempts"][0]
+                if lesson_attempt["is_negative"]:
+                    attempt_result = "К сожалению, в работе нашлись ошибки."
+                else:
+                    attempt_result = "Преподавателю все понравилось, можно приступать к следующему уроку!"
+
+                message = f"""
+                    У вас проверили работу «{lesson_attempt["lesson_title"]}»
+                    {lesson_attempt["lesson_url"]}
+
+                    {attempt_result}
+                    """
+
+                bot.send_message(
+                    chat_id=chat_id, text=message
+                )
+
+                payload = {"timestamp": lesson_attempt["timestamp"]}
+                continue
+            else:
+                payload = {"timestamp": response["timestamp_to_request"]}
+                continue
+        except requests.exceptions.ReadTimeout:
+            exit("Server response timeout have changed, update 'REQUEST_TIMEOUT' setting.")
+        except requests.exceptions.ConnectionError:
+            continue
+
+
+if __name__ == "__main__":
+    main()
